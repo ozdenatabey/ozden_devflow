5dd790c7764727d7f1114c6acb42f6afb99c03b9
ozdenatabey <ozdenatabey44@gmail.com>
2024-11-05 15:46:57 +0300

    MobileNav and NavLinks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,4 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 const Home = async () => {
   const name: string = "ozden";
@@ -14,15 +12,6 @@ const Home = async () => {
       <h1 className="h2-semibold font-space-grotesk text-primary-500">
         Welcome to the world of Next JS
       </h1>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form>
     </>
   );
 };
