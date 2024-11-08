7aa099f004689d8a9fbf8fd84559d3e04d4070bc
ozdenatabey <ozdenatabey44@gmail.com>
2024-11-08 16:22:55 +0300

    Implement the UI of the Question Form

--- constants/routes.ts
+++ constants/routes.ts
@@ -2,7 +2,7 @@ const ROUTES = {
   HOME: "/",
   SIGN_IN: "/sign-in",
   SIGN_UP: "/sign-up",
-  ASK_QUESTION: "/ask_question",
+  ASK_QUESTION: "/ask-question",
   PROFILE: (id: string) => `/profile/${id}`,
   QUESTION: (id: string) => `/questions/${id}`,
   TAGS: (id: string) => `/tags/${id}`,
