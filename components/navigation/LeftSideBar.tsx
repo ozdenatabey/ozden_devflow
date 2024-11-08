cdb7a5aa8cad409c13bad309da32a6489d2b270d
ozdenatabey <ozdenatabey44@gmail.com>
2024-11-08 14:31:55 +0300

    LeftSideBar fixed small device visibilty

--- components/navigation/LeftSideBar.tsx
+++ components/navigation/LeftSideBar.tsx
@@ -9,7 +9,7 @@ import { Button } from "../ui/button";
 
 const LeftSideBar = () => {
   return (
-    <section className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 flex h-screen w-fit flex-col justify-between overflow-y-auto border-r p-6 pt-36 lg:w-[266px]">
+    <section className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 flex h-screen w-fit flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
       <div className="flex flex-1 flex-col gap-6">
         <NavLinks />
       </div>
